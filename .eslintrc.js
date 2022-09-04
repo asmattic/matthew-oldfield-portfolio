31813e7bc2a675b89453eeee86d880ad45d7a91e
Matt Oldfield <mattoldfieldweb@gmail.com>
2022-09-04 18:00:16 -0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  extends: [
+    'eslint-config-kentcdodds',
+    'eslint-config-kentcdodds/jest',
+    'eslint-config-kentcdodds/jsx-a11y',
+    'eslint-config-kentcdodds/react',
+    'next/core-web-vitals'
+  ],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  rules: {
+    'no-console': 'off',
+
+    // meh...
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/sort-type-union-intersection-members': 'off',
+    'jsx-a11y/media-has-caption': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/alt-text': 'off', // it's not smart enough...
+    '@babel/new-cap': 'off',
+    'react/jsx-filename-extension': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+
+    // I can't figure these out:
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+
+    // enable these again someday:
+    '@typescript-eslint/no-unsafe-argument': 'off',
+
+    // this one isn't smart enough for our "~/" imports
+    'import/order': 'off',
+
+    // for CatchBoundaries
+    '@typescript-eslint/no-throw-literal': 'off',
+    'testing-library/no-await-sync-events': 'off',
+
+    // this auto-fixes and it's nice to have types and actual stuff separate
+    '@typescript-eslint/consistent-type-imports': 'warn',
+  },
+}
